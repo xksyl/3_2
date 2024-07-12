bcad57ee1382d380af0db4a36da3bf06fd7aee81
xksyl <egorka2004212@gmail.com>
2024-07-12 12:52:28 +0500

    Add files via upload

---
+++ module_3_2.py
@@ -0,0 +1,21 @@
+def check_for_email(a):
+    chars = ('.com', '.ru', '.net')
+
+    if a.count('@') == 1 and a[0] != '@' and a.endswith(chars):
+        return True
+    else:
+        return False
+def send_email (massage, recipient,*, sender="university.help@gmail.com"):
+    if not check_for_email(recipient) or not check_for_email(sender):
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+    elif sender == recipient:
+        print('Нельзя отправить письмо самому себе!')
+    elif sender == 'university.help@gmail.com':
+        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
+    else:
+        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}')
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
